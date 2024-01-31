83a77121c7c3f09c39ddbe0a1c43eed3d78eb625
Matthew Montejo <montejododger@yahoo.com>
2024-01-30 16:12:18 -0800

    reverseWord'

--- leetCode75_problems.js
+++ leetCode75_problems.js
@@ -244,7 +244,7 @@ s consist of printable ASCII characters.
 */
 
 var reverseVowels = function (s) {
-    const vowels = "aeiou";
+    const vowels = "aeiouAEIOU";
     let word = s.split("");
 
     let front = 0;
@@ -265,5 +265,61 @@ var reverseVowels = function (s) {
     }
     return word.join("");
 };
+
+// ################################################################################################################
+
+/*
+
+Code
+Testcase
+Testcase
+Test Result
+151. Reverse Words in a String
+Medium
+Topics
+Companies
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+*/
+
+
+//  Time and space - 0(n)
+var reverseWords = function(s) {
+    let words = trimmed.split(" ").filter(word => word.length !== 0)
+    return words.reverse().join(" ")
+};
+
 
 // ################################################################################################################
